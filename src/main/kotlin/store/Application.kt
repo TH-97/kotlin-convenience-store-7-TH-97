4299f3bf75303aaa089c025c7acfaf7b29dd6e96
TH_97 <116719843+TH-97@users.noreply.github.com>
2024-11-09 17:26:38 +0900

    fix(model) Products를 NormalProducts로 변경

--- src/main/kotlin/store/Application.kt
+++ src/main/kotlin/store/Application.kt
@@ -1,5 +1,27 @@
 package store
 
 fun main() {
-    // TODO: 프로그램 구현
+//    readProductsFromFile("src/main/resources/products.md")
+//    displayProducts(a)
 }
+
+//fun readProductsFromFile(filePath: String): List<NormalProducts> {
+//    return File(filePath).useLines { lines ->
+//        lines.drop(1)
+//            .filter { it.isNotBlank() }
+//            .map { line -> parseProduct(line) }
+//            .toList()
+//    }
+//}
+//
+//fun parseProduct(line: String): NormalProducts {
+//    val tokens = line.split(",").map { it.trim() }
+//    return NormalProducts(
+//        name = tokens[0],
+//        price = tokens[1].toInt(),
+//        quantity = tokens[2].toInt(),
+//        promotion = tokens[3].takeIf { it != "null" }
+//    )
+//}
+
+
---
+++ src/main/kotlin/store/model/NormalProducts.kt
@@ -0,0 +1,16 @@
+package store.model
+
+class NormalProducts(
+    private val name: String,
+    private val price: Int,
+    private val quantity: Int,
+    private val promotion: String?
+) {
+    fun display(): String {
+        return if (quantity > 0) {
+            "$name ${price}원 ${quantity}개${if (!promotion.isNullOrEmpty()) " $promotion" else ""}"
+        } else {
+            "$name ${price}원 재고 없음"
+        }
+    }
+}
